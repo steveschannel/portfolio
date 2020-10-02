eeb21e49c1e685cb7d3c3a00df94aa626cb5c425
steveschannel <kawarii1@gmail.com>
2020-10-02 17:29:30 -0400

    build upload
    1st ver prod

---
+++ build/precache-manifest.d2f6952b3965534f17153e200c3b3a51.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd76bf2a5b526419f09372fc9c1c4c24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "312d1f9105ac4677f310",
+    "url": "/static/css/main.e9448d43.chunk.css"
+  },
+  {
+    "revision": "98cfbcce47e07b6b7934",
+    "url": "/static/js/2.a88f9c9b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a88f9c9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "312d1f9105ac4677f310",
+    "url": "/static/js/main.91d8d0ca.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "e4abba3845317210ddbcdb9d4a392115",
+    "url": "/static/media/1.e4abba38.png"
+  },
+  {
+    "revision": "e8828d8ee730c31d3e108e3c90897a79",
+    "url": "/static/media/PDF Design Portfolio.e8828d8e.pdf"
+  },
+  {
+    "revision": "7b380a776172fee8ebf023963ff07231",
+    "url": "/static/media/Resume.7b380a77.pdf"
+  }
+]);
